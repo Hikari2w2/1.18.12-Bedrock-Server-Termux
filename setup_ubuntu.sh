7010955c5e83d5773792956a9be52783014c9494
Debojit Santra <debojitsantra48@gmail.com>
2025-03-21 20:40:03 +0530

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,5 +1,8 @@
-apt update -y
-apt upgrade -y
-
-pkg install proot-distro
-proot-distro install ubuntu
+apt update -y 
+apt upgrade -y  
+pkg install proot-distro 
+proot-distro install ubuntu 
+echo "proot-distro login ubuntu" > pd 
+chmod +x pd 
+mv pd /data/data/com.termux/files/usr/bin 
+echo "Now write pd and press enter"
