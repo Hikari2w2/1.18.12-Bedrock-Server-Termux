cae7de419ad016d410c9663b0a3c19b8110bb5e9
Debojit Santra <debojitsantra48@gmail.com>
2025-03-21 21:12:27 +0530

    Add files via upload

---
+++ setup_env.sh
@@ -0,0 +1,27 @@
+apt update -y
+apt upgrade -y
+apt install git box64 -y
+apt install sudo -y
+
+sudo apt-key del '16AC CC32 BD41 5DCC 6F00  D548 DA6C D75E C283 9680'
+sudo rm /etc/apt/sources.list.d/playit-cloud.list
+sudo apt update -y
+
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+sudo apt update -y
+
+apt install playit -y
+
+
+wget https://filebin.net/7smdai2au1tobhr8/server.zip
+unzip server.zip
+cd Xboyes
+chmod +x bedrock_server
+chmod +x start.sh
+cd
+
+
+
+
+
